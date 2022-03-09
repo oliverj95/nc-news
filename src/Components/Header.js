24d05bb9f45c9252fff779d3f3106f457b37cc8d
oliverj95 <oliverjim@hotmail.co.uk>
2022-03-09 16:13:02 +0000

    conditional rendering of login button and logged in user

--- src/Components/Header.js
+++ src/Components/Header.js
@@ -1,15 +1,29 @@
+import { useContext } from "react";
 import { Link } from "react-router-dom";
+import { UserContext } from "../Contexts/User";
 import "../styles/Header.css"
 
 const Header = () => {
+    const {setLoggedInUser, loggedInUser, isLoggedIn} = useContext(UserContext)
+    const handleLogout = () => {
+        setLoggedInUser({})
+    }
+    
     return (
         <header>
             <Link to="/articles">
             <h1 className="main-title"> NC News</h1>
             </Link>
-            <Link to="/Login">
+            {isLoggedIn ? <div className="navLogin"> 
+            <img id="user-avatar" src={loggedInUser.avatar_url} />
+            <h5>
+                {loggedInUser.username}
+            </h5>
+            <button className="logoutButton" onClick={handleLogout}>Logout</button>
+            </div> : <Link to="/Login">
             <h4 className="Login"> Login </h4>
-            </Link>
+            </Link>}
+            
         </header>
 
     )
